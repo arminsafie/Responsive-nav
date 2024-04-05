91dd753098f5deb0711d735131b7453cfb74c2cc
arminsafie <arminsafaie92@gmail.com>
2024-04-05 08:25:05 +0330

    bug fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,5 +2,5 @@ const phoneNavBtn = document.getElementById("phone-nav-btn");
 const phoneHidden = document.getElementById("phone-hidden");
 console.log(phoneNavBtn);
 phoneNavBtn.addEventListener("click", () => {
-  phoneHidden.classList.toggle("phone-hidden");
+  phoneHidden.classList.toggle("phone-hidden-part-menu--active");
 });
